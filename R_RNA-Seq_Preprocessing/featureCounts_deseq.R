0d804aa383ae9ec904b47669c669381f53dd456f
SodzieR <xsodziero@gmail.com>
2021-09-02 13:02:09 +0200

    added more stuff and miRNAs

--- R_RNA-Seq_Preprocessing/featureCounts_deseq.R
+++ R_RNA-Seq_Preprocessing/featureCounts_deseq.R
@@ -1,58 +1,104 @@
 
-setwd('/Users/andrzejeljaszewicz/Erasmus_UP/nextflowTranscriptomics/nextflowScripts')
+# Dependencies ------------------------------------------------------------
+suppressPackageStartupMessages(library(magrittr))
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(tibble))
+suppressPackageStartupMessages(library(stringr))
+suppressPackageStartupMessages(library(DESeq2))
+suppressPackageStartupMessages(library(readr))
 
-library(magrittr)
-library(dplyr)
-library(tibble)
-library(stringr)
-library(DESeq2)
+# Setup -------------------------------------------------------------------
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
 
-meta <- read.csv('/Users/andrzejeljaszewicz/Erasmus_UP/liam_data_dge_analysis/sampleTable2.csv')
+# Read merged featureCounts matrix in
+merged_featureCounts_matrix <-
+  '/Users/andrzejeljaszewicz/Erasmus_UP/scripts_main/R_miRNA_main/miRNA_Witek_raw_counts.csv'
 
-meta$condition <- factor(meta$condition, levels = c('NonAsthma', 'Asthma'))
-
-featurecounts_matrix <- read.csv('/Users/andrzejeljaszewicz/Erasmus_UP/nextflowTranscriptomics/nextflowScripts/featureCounts/featureCounts_matrix.txt',
-         sep = "\t", skip = 1L) %>% 
-  dplyr::select(-c('Chr','Start','End','Strand','Length')) %>% 
+# Read merged featureCounts matrix in
+featureCounts_matrix <-
+  read_delim(
+    merged_featureCounts_matrix
+  ) %>%
   column_to_rownames(var = 'Geneid')
 
-colnames(featurecounts_matrix) <- meta$samples
+# Trim the colnames if neeeded
+colnames(featureCounts_matrix) <- stringr::str_subset(colnames(featureCounts_matrix), '.fastq.gz_fastp.fastq.gzAligned.out.bam')
 
-dds <- DESeqDataSetFromMatrix(countData = featurecounts_matrix,
+# Read meta or create the condition
+# Meta file has to have 'samples' and 'condition' columns
+meta_file <-
+  ''
+
+# Check if meta file already exist and set factors, but if not do it manually
+if (file.exists(meta_file)) {
+  meta <- read.csv(meta_file)
+  # Set factor leves
+  meta$condition <-
+    factor(meta$condition, levels = c('NonAsthma', 'Asthma'))
+} else {
+  meta <- data.frame(samples = colnames(featureCounts_matrix),
+                     condition = factor(rep(1:2, each = 100), levels = c(1,2)))
+}
+
+# Specify outpit directories and file names
+de_results_out <- '/Users/andrzejeljaszewicz/Erasmus_UP/scripts_main/R_miRNA_main/miRNA_Witek_de_results.csv'
+
+normalised_counts_out <- '/Users/andrzejeljaszewicz/Erasmus_UP/scripts_main/R_miRNA_main/miRNA_Witek_normalised_counts.csv'
+
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
+
+# Function for already merged featureCounts matrix ------------------------
+
+#featurecounts_matrix <- read.csv('/Users/andrzejeljaszewicz/Erasmus_UP/nextflowTranscriptomics/nextflowScripts/featureCounts/featureCounts_matrix.txt',
+#         sep = "\t", skip = 1L) %>%
+#  dplyr::select(-c('Chr','Start','End','Strand','Length')) %>%
+#  column_to_rownames(var = 'Geneid')
+
+# Load --------------------------------------------------------------------
+
+# Assign colnames as samples
+colnames(featureCounts_matrix) <- meta$samples
+
+# Create DESeq2 object
+dds <- DESeqDataSetFromMatrix(countData = featureCounts_matrix,
                               colData = meta,
-                              design = ~condition)
+                              design = ~ condition)
 
+# Prefilter for low count genes
 keep <- rowSums(counts(dds)) >= 20
 
-dds_f <- dds[keep,]
+# Perform filtering
+dds_f <- dds[keep, ]
 
+# Perform DE
 dds_deseq <- DESeq(dds_f)
 
+# Obtain DE results from DE object
 res <- results(dds_deseq)
 
+# Summarize
 summary(res)
 
+# Normalized counts using DESeq2's median of ratios method allowing for sample-sample comparisions
+# (only other tool capable of this kind of normalization is EdgeR)
 dds_norm <- estimateSizeFactors(dds)
-counts_norm <- counts(dds_norm, normalized=TRUE)
+counts_norm <- counts(dds_norm, normalized = TRUE)
 
-res_sroted <- res %>% 
-  data.frame() %>% 
-  rownames_to_column(var = 'id') %>% 
+# Prepare DE results for export
+res_sroted <- res %>%
+  data.frame() %>%
+  rownames_to_column(var = 'id') %>%
   arrange(id)
 
-counts_norm_sorted <- counts_norm %>% 
-  data.frame() %>% 
-  rownames_to_column(var = 'id') %>% 
+# Prepare normalized matrix for export
+counts_norm_sorted <- counts_norm %>%
+  data.frame() %>%
+  rownames_to_column(var = 'id') %>%
   arrange(id)
 
-write.csv(res_sroted, file = '/Users/andrzejeljaszewicz/Erasmus_UP/liam_data_dge_analysis/deseq_featureCounts_dge.csv')
+# Export data as .csv
+write_csv(res_sroted, file = de_results_out)
 
-write.csv(counts_norm_sorted, file = '/Users/andrzejeljaszewicz/Erasmus_UP/liam_data_dge_analysis/deseq_featureCounts_normalised_counts.csv')
-
-
-
-
-
-
-
-
+write_csv(counts_norm_sorted, file = normalised_counts_out)
--- R_RNA-Seq_main/featureCounts_merge.R
+++ R_RNA-Seq_main/featureCounts_merge.R
@@ -1,19 +1,48 @@
+
+# Setup -------------------------------------------------------------------
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
+
+featureCounts_directory <- 
+  '/Users/andrzejeljaszewicz/Erasmus_UP/scripts_main/R_miRNA_main/featureCounts_Witek_mirnas'
+
+output_file_dir <- 
+  '/Users/andrzejeljaszewicz/Erasmus_UP/scripts_main/R_miRNA_main/miRNA_Witek_raw_counts.csv'
+
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
+
+# Dependencies ------------------------------------------------------------
 library(purrr)
 library(tidyverse)
 library(readr)
 
-setwd("/Users/andrzejeljaszewicz/Erasmus_UP/mirnas/input_mirnas/mirna_input_featureCounts")
+# Load --------------------------------------------------------------------
 
-f_files<- list.files("/Users/andrzejeljaszewicz/Erasmus_UP/mirnas/input_mirnas/mirna_input_featureCounts")
+# Change working dir so the listed files can be found
+setwd(featureCounts_directory)
 
-read_in_feature_counts<- function(file){
-  cnt<- read_tsv(file, col_names =T, comment = "#")
-  cnt<- cnt %>% dplyr::select(-Chr, -Start, -End, -Strand, -Length)
+# List files in directory
+f_files <-
+  list.files(
+    featureCounts_directory
+  )
+
+# Create function to merge featureCounts matrices for individual samples
+read_in_feature_counts <- function(file) {
+  cnt <- read_tsv(file, col_names = T, comment = "#")
+  cnt <- cnt %>% dplyr::select(-Chr,-Start,-End,-Strand,-Length)
   return(cnt)
 }
 
-raw_counts<- map(f_files, read_in_feature_counts)
+# Merge and create matrix
+raw_counts <- map(f_files, read_in_feature_counts)
 
-raw_counts_df<- purrr::reduce(raw_counts, inner_join) 
+# Create object matrix
+raw_counts_df <- purrr::reduce(raw_counts, inner_join)
 
-write_csv(raw_counts_df, '/Users/andrzejeljaszewicz/Erasmus_UP/mirnas/input_mirnas/input_mirnas_liam_featureCounts_matrix.txt')
+# Save file as .csv
+write_csv(
+  raw_counts_df,
+  output_file_dir
+)
